aaacd8efd041a7485eb2d33cb681afb5db982514
raulandresguillen <117307973+raulandresguillen@users.noreply.github.com>
2023-08-05 20:06:55 -0300

    se agrego nosotros

--- script.js
+++ script.js
@@ -1,23 +1,83 @@
-let display = document.getElementById('display');
-let expression = '';
+// let display = document.getElementById('display');
+// let expression = '';
+
+// function appendToDisplay(value) {
+//   expression += value;
+//   display.value = expression;
+// }
+
+// function clearDisplay() {
+//   expression = '';
+//   display.value = '';
+// }
+
+// function calculate() {
+//   try {
+//     let result = eval(expression);
+//     display.value = result;
+//     expression = result.toString();
+//   } catch (error) {
+//     display.value = 'Error';
+//     expression = '';
+//   }
+// }
+
+//calculador 2
+
+// let displayValue = '';
+
+// function appendToDisplay(value) {
+//   displayValue += value;
+//   updateDisplay();
+// }
+
+// function updateDisplay() {
+//   document.getElementById('display').value = displayValue;
+// }
+
+// function clearDisplay() {
+//   displayValue = '';
+//   updateDisplay();
+// }
+
+// function calculate() {
+//   try {
+//     const result = eval(displayValue);
+//     displayValue = result.toString();
+//     updateDisplay();
+//   } catch (error) {
+//     displayValue = 'Error';
+//     updateDisplay();
+//   }
+// }
+
+
+// calculador 3
+
+let displayValue = '';
 
 function appendToDisplay(value) {
-  expression += value;
-  display.value = expression;
+  displayValue += value;
+  updateDisplay();
+}
+
+function updateDisplay() {
+  document.getElementById('display').value = displayValue;
 }
 
 function clearDisplay() {
-  expression = '';
-  display.value = '';
+  displayValue = '';
+  updateDisplay();
 }
 
 function calculate() {
   try {
-    let result = eval(expression);
-    display.value = result;
-    expression = result.toString();
+    const result = eval(displayValue);
+    displayValue = result.toString();
+    updateDisplay();
   } catch (error) {
-    display.value = 'Error';
-    expression = '';
+    displayValue = 'Error';
+    updateDisplay();
   }
 }
+
